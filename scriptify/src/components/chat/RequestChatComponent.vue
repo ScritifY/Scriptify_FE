9ea6def3ee39184210506a6b127f986ee4155a3c
unknown <dhsmf4245@gmail.com>
2024-11-21 14:05:54 +0900

    design: 채팅 인풋, 아웃풋 디자인 수정

--- scriptify/src/components/chat/RequestChatComponent.vue
+++ scriptify/src/components/chat/RequestChatComponent.vue
@@ -1,29 +1,39 @@
 <!-- RequestChatComponent.vue -->
 <template>
     <div class="request-chat">
-      <div class="chat-bubble">{{ message }}</div>
+      <div class="chat-bubble">
+        <div >Time: {{ `${message.time}` }} </div>
+        <div >Space: {{ message.space }}</div>
+        <div >Genre: {{ message.genre }}</div>
+      </div>
     </div>
   </template>
   
   <script setup>
   defineProps({
-    message: String,
+    message: Object,
   });
   </script>
   
   <style scoped>
   .request-chat {
-    text-align: right;
+    display: flex; /* 플렉스박스 사용 */
+    flex-direction: column;
+    align-items: flex-end; /* 모든 요소를 오른쪽 정렬 */
+
   }
   
   .chat-bubble {
-    max-width: 70%;
+    max-width: 50%;
     padding: 10px;
     /* margin: 5px; */
     margin-left: 20%;
     border-radius: 10px;
-    background-color: #d0f4de;
+    background-color: white;
     word-wrap: break-word;
+    text-align: left;
+    color: #FFB182	;
   }
+
   </style>
   
